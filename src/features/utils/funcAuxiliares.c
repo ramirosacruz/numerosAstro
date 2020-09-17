f191e6437f483532b252876fc933821fd3ea2bcc
NicoCarson <nicko_0323@hotmail.com>
2020-09-17 14:04:07 -0300

    Le puse un comentario

--- src/features/utils/funcAuxiliares.c
+++ src/features/utils/funcAuxiliares.c
@@ -5,4 +5,4 @@ char enteroACaracter(int numero){
 int caracterAEntero(char caracter){
     int x = caracter + 0;
     return x - 48;
-}
+}//Esta funcion te devuelve el ASCII del caracter menos 48 asi que si pones letras o simbolos tambien te va a devolver un numero tenelo en cuenta
