9f604e10f803fde6c8605b3e466958d96be67a62
NicoCarson <nicko_0323@hotmail.com>
2020-09-25 22:49:05 -0300

    Update arithmetic.c

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -1,2 +1,55 @@
+
+/*
+11. Sumar : NumeroAstronomico × NumeroAstronomico → NumeroAstronomico
+12. SonIguales : NumeroAstronomico × NumeroAstronomico → Boolean
+13. EsMenor : NumeroAstronomico × NumeroAstronomico → Boolean
+*/
 #include <stdio.h>
-//Hola
+#include <stdlib.h>
+
+typedef struct {
+const char* entero;
+int longitudError;
+} NumeroAstronomico;
+
+int caracterAEntero(char caracter){
+    int x = caracter + 0;
+    return x - 48;
+}
+
+int longitudDeArreglo(const char* puntero){
+    int longitud = 0;
+    while(*puntero != '\0'){
+        longitud ++;
+        *puntero ++;
+    }
+    return longitud;
+}
+
+int esMenor(NumeroAstronomico primerNro, NumeroAstronomico segundoNro){
+    int bool = 1;
+    if(primerNro.longitudError < segundoNro.longitudError)
+        return bool;
+    else{
+        if(primerNro.longitudError == segundoNro.longitudError){
+            if(*primerNro.entero < *segundoNro.entero)
+                return bool;    
+        } 
+        return bool = 0;    
+    }
+}
+
+int sonIguales(NumeroAstronomico primerNro, NumeroAstronomico segundoNro){
+    int bool = 0;
+    if(primerNro.longitudError != segundoNro.longitudError)
+        return bool;
+    else{
+        while(*primerNro.entero != '\0'){
+            if(*primerNro.entero != *segundoNro.entero)
+                return bool;
+            *primerNro.entero ++;
+            *segundoNro.entero ++;
+        }
+        return bool = 1;
+    }
+}
