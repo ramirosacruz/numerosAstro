284c66dbd7974ef6c55f399677861dd2836a2625
NicoCarson <nicko_0323@hotmail.com>
2020-10-26 19:52:19 -0300

    crearDesdeCadena y printError

--- src/numeroAstronomico.c
+++ src/numeroAstronomico.c
@@ -2,7 +2,17 @@
 #include <arithmetic.h>
 #include <general.h>
 
-NumeroAstronomico crearDesdeCadena(char* cadena);
+NumeroAstronomico crearDesdeCadena(char* cadena){
+    NumeroAstronomico numeroNuevo;
+    numeroNuevo.entero = cadena;
+    if (esError(numeroNuevo)){
+        printError(numeroNuevo);
+        numeroNuevo.longitudError = getTipoDeError(numeroNuevo);
+        return numeroNuevo;
+    }
+    numeroNuevo.longitudError = longitudDeArreglo(numeroNuevo.entero);
+    return numeroNuevo;
+}
 
 NumeroAstronomico crearDesdeCifraSeguidaDeCeros(int cifra, int cantCeros);
 
