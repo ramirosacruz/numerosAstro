72042817afa216f5902c7881e925d5255406d5f0
NicoCarson <nicko_0323@hotmail.com>
2020-11-04 21:24:51 -0300

    @return

--- src/errorHandling.c
+++ src/errorHandling.c
@@ -13,22 +13,35 @@ Esta operación equivale a: EsCadenaNula ∨ EsCadenaInvalida ∨ EsOverflow
 #include <stdlib.h>
 #include "./errorHandling.h"
 
-int esSecuenciaNula(NumeroAstronomico nro){
-    if(longitudDeArreglo(nro.entero))
+/**
+ *@return A partir de un NA se fija si este es nulo o existe y devuelve TRUE(1) si existiese o FALSE(0) si no
+ */
+int esSecuenciaNula(NumeroAstronomico nro)
+{
+    if (longitudDeArreglo(nro.entero))
         return FALSE;
     else
-        return TRUE;  
+        return TRUE;
 }
 
-int esOverFlow(NumeroAstronomico nro){
-    if(longitudDeArreglo(nro.entero) <= 102)
+/**
+ *@return A partir de un NA se fija si este excede la longitud maxima de caracteres(100) y devuelve TRUE(1) si la excede o FALSE(0) si no
+ */
+int esOverFlow(NumeroAstronomico nro)
+{
+    if (longitudDeArreglo(nro.entero) <= 102)
         return FALSE;
     else
-        return TRUE;  
+        return TRUE;
 }
 
-int esSecuenciaInvalida(NumeroAstronomico nro){
-    while(*nro.entero != '\0'){
+/**
+ *@return A partir de un NA se fija si este contiene un caracter invalido(cualquiera que no fuese un digito) y devuelve TRUE(1) si lo contiene o FALSE(0) si no
+ */
+int esSecuenciaInvalida(NumeroAstronomico nro)
+{
+    while (*nro.entero != '\0')
+    {
         int entero;
         entero = caracterAEntero(*nro.entero);
         if (entero < 0 || entero > 9)
@@ -38,45 +51,58 @@ int esSecuenciaInvalida(NumeroAstronomico nro){
     return FALSE;
 }
 
-int esError(NumeroAstronomico nro){
+/**
+ *@return A partir de un NA se fija si este tiene algun tipo de error(secuencia nula, secuencia invalida o overflow) y devuelve TRUE(1) si lo tiene o FALSE(0) si no
+ */
+int esError(NumeroAstronomico nro)
+{
     int error1 = esSecuenciaNula(nro);
     int error2 = esSecuenciaInvalida(nro);
     int error3 = esOverFlow(nro);
-    if(error1 + error2 + error3)
+    if (error1 + error2 + error3)
         return TRUE;
     else
-        return FALSE; 
+        return FALSE;
 }
 
-int getTipoDeError(NumeroAstronomico nro){
-    if(esSecuenciaNula(nro))
+/**
+ *@return A partir de un NA se fija si este contiene algun error y devuelve SECUENCIA_NULA(-1), SECUENCIA_INVALIDA(-2), OVER_FLOW(-3), DOBLE_ERROR(-4)(este se produce cuando hay tanto secuencia invalida como overflow) o FALSE(0) si no se encontrase error
+ */
+int getTipoDeError(NumeroAstronomico nro)
+{
+    if (esSecuenciaNula(nro))
         return SECUENCIA_NULA;
-    if(esOverFlow(nro)){
-        if(esSecuenciaInvalida(nro))
+    if (esOverFlow(nro))
+    {
+        if (esSecuenciaInvalida(nro))
             return DOBLE_ERROR;
         else
             return OVER_FLOW;
     }
-    if(esSecuenciaInvalida(nro))
+    if (esSecuenciaInvalida(nro))
         return SECUENCIA_INVALIDA;
     return FALSE;
 }
- 
-void printError(NumeroAstronomico numero){
+
+/**
+ *@return A partir de un NA se fija que tipo de error tiene y muestra por pantalla un mensaje de error con el error correspondiente
+ */
+void printError(NumeroAstronomico numero)
+{
     switch (numero.longitudError)
     {
     case -1:
         printf("Error: El Numero ingresado es nulo \n");
         break;
-    
+
     case -2:
         printf("Error: El Numero ingresado tiene caracteres invalidos \n");
         break;
-    
+
     case -3:
         printf("Error: El Numero ingresado supera lo 100 digitos \n");
         break;
-    
+
     case -4:
         printf("Error: El Numero ingresado tiene caracteres invalidos y supera los 100 digitos \n");
         break;
--- src/numeroAstronomico.c
+++ src/numeroAstronomico.c
@@ -53,14 +53,16 @@ char *obtenerSoloValor(NumeroAstronomico nro)
     return ptr;
 }
 
-
+/**
+ *@return Recibe un NA y devuelve sus banderas
+ */
 char *obtenerBanderas(NumeroAstronomico nro)
 {
     char *ptr;
 
     ptr = (char *)malloc(2 * sizeof(char)); // allocate memory to store 10 characters
 
-     for (int i = 0; i < 2; i++)
+    for (int i = 0; i < 2; i++)
     {
         ptr[i] = *nro.entero;
         nro.entero++;
@@ -70,7 +72,7 @@ char *obtenerBanderas(NumeroAstronomico nro)
 }
 
 /**
- * @return 
+ * @return Recibe una cadena de caracteres y si es valida genera un NA con el contenido de esta
  * */
 NumeroAstronomico crearDesdeCadena(char *cadena)
 {
@@ -90,7 +92,7 @@ NumeroAstronomico crearDesdeCadena(char *cadena)
 }
 
 /**
- * @return 
+ * @return Apartir de una cifra y una cantidad genera un NA al cual se le pone la cifra dada por delante y seguida de esta viene una serie de ceros dada por la cantidad
  * */
 NumeroAstronomico crearDesdeCifraSeguidaDeCeros(int cifra, int cantCeros)
 {
@@ -121,14 +123,20 @@ NumeroAstronomico crearDesdeCifraSeguidaDeCeros(int cifra, int cantCeros)
 }
 
 /**
- * @return 
+ * @return Genera un NA aleatorio tanto es su longitud(100 como maximo) como en su contenido
  * */
-NumeroAstronomico crearAleatorio(){
+NumeroAstronomico crearAleatorio()
+{
     srand(time(NULL));
     int longitud = (rand() % 100) + 1;
     char *cadenaAux = (char *)malloc(longitud * sizeof(char));
     char *punt = cadenaAux;
-    for(int i=0; i < longitud; i++)
+    do
+    {
+        *punt = enteroACaracter(rand() % 10);
+    } while (!*punt);
+    punt++;
+    for (int i = 0; i < longitud - 1; i++)
     {
         *punt = enteroACaracter(rand() % 10);
         punt++;
@@ -141,9 +149,10 @@ NumeroAstronomico crearAleatorio(){
 }
 
 /**
- * @return 
- * */ 
-NumeroAstronomico scanFlujoDeTexto(FILE *flujo){
+ * @return Genera un NA apartir del contenido de un archivo dado, este debe contener el numero con el formato: secuenciadedigitos#
+ * */
+NumeroAstronomico scanFlujoDeTexto(FILE *flujo)
+{
     char *cadenaAux = (char *)malloc(sizeof(flujo));
     char *punt = cadenaAux;
     do
@@ -151,13 +160,13 @@ NumeroAstronomico scanFlujoDeTexto(FILE *flujo){
         *punt = fgetc(flujo);
         punt++;
         *punt = fgetc(flujo);
-        ungetc(*punt,flujo);
+        ungetc(*punt, flujo);
     } while (*punt != '#');
     *punt = '\0';
     NumeroAstronomico numeroNuevo;
     numeroNuevo.longitudError = longitudDeArreglo(cadenaAux);
     numeroNuevo.entero = generarCadenaDeNumeroAstronomico(cadenaAux, 0, 0);
-    if(esError(numeroNuevo))
+    if (esError(numeroNuevo))
     {
         numeroNuevo.longitudError = getTipoDeError(numeroNuevo);
         printError(numeroNuevo);
@@ -165,64 +174,67 @@ NumeroAstronomico scanFlujoDeTexto(FILE *flujo){
     fclose(flujo);
     return numeroNuevo;
 }
- 
+
 /**
- * @return 
+ * @return Recibe un numero, una cantidad de grupos en la primer linea  y un flujo igualado a stdout por lo que apunta a la pantalla de la consola. Su funcion es mostrar el numero en consola de forma tal que aparecen en la primer linea la cantidad dada de grupos de 3 numeros (salvo el primero que puede ser de 3, 2 o 1) separados por un punto, en las siguientes lineas se muestran una cantidad de 1 menos que la primera de grupos por linea (siempre que la longitud lo permita), y termina con una linea con la cantidad de grupos sobrantes si estos existiesen. 
  * */
-FILE *mostrar(NumeroAstronomico nro, int grupoEnPrimerLinea,FILE *flujo){
+FILE *mostrar(NumeroAstronomico nro, int grupoEnPrimerLinea, FILE *flujo)
+{
     nro.entero += 2;
     int modulo = nro.longitudError % 3;
-    if(!modulo)
+    if (!modulo)
         modulo = 3;
-    int longitudPrimerLinea = (grupoEnPrimerLinea - 1)*3 + modulo;
-    if(longitudPrimerLinea > nro.longitudError || (grupoEnPrimerLinea == 1 && nro.longitudError > 3))
+    int longitudPrimerLinea = (grupoEnPrimerLinea - 1) * 3 + modulo;
+    if (longitudPrimerLinea > nro.longitudError || (grupoEnPrimerLinea == 1 && nro.longitudError > 3))
     {
         printf("Error: Grupo/s en primera linea exceden la longitud del numero o es igual a 1. \n");
         return flujo;
     }
     nro.entero = mostrarLinea(flujo, nro.entero, longitudPrimerLinea);
-    int gruposSobrantes = (nro.longitudError - longitudPrimerLinea)/3;
+    int gruposSobrantes = (nro.longitudError - longitudPrimerLinea) / 3;
     if (!gruposSobrantes)
     {
-        fputc('\n',flujo);
+        fputc('\n', flujo);
         return flujo;
     }
-    fputc('.',flujo);
-    fputc('\n',flujo);
+    fputc('.', flujo);
+    fputc('\n', flujo);
     modulo = gruposSobrantes % (grupoEnPrimerLinea - 1);
     int lineasSobrantes = (gruposSobrantes - modulo) / (grupoEnPrimerLinea - 1);
-    for(int i = 0; i < lineasSobrantes; i++)
+    for (int i = 0; i < lineasSobrantes; i++)
     {
-        nro.entero = mostrarLinea(flujo, nro.entero, (grupoEnPrimerLinea - 1)*3);
-        if(i == lineasSobrantes - 1 && !modulo){
-            fputc('\n',flujo);
+        nro.entero = mostrarLinea(flujo, nro.entero, (grupoEnPrimerLinea - 1) * 3);
+        if (i == lineasSobrantes - 1 && !modulo)
+        {
+            fputc('\n', flujo);
             return flujo;
         }
-        fputc('.',flujo);
-        fputc('\n',flujo);
+        fputc('.', flujo);
+        fputc('\n', flujo);
     }
-    nro.entero = mostrarLinea(flujo, nro.entero, modulo*3);
-    fputc('\n',flujo);
+    nro.entero = mostrarLinea(flujo, nro.entero, modulo * 3);
+    fputc('\n', flujo);
     return flujo;
 }
 
 /**
- * @return 
+ * @return Recibe un flujo igualado a stdout, un puntero a char y una longitud, y muestra una linea la cantidad de numeros dada por la longitud comenzando por donde apunte el puntero, estos estan separados por un punto en grupos de 3 (salvo el primero que puede ser 3, 2 o 1)
  * */
-const char* mostrarLinea(FILE *flujo,const char *ptr,int longitud){
-    int modulo = longitud%3;
-    if(!modulo)
+const char *mostrarLinea(FILE *flujo, const char *ptr, int longitud)
+{
+    int modulo = longitud % 3;
+    if (!modulo)
         modulo = 3;
     int aux = 3 - modulo;
     longitud = longitud + aux;
-    fputc('\t',flujo);
-    fputc('\t',flujo);
-    for(int i = aux; i < longitud; i++)
+    fputc('\t', flujo);
+    fputc('\t', flujo);
+    for (int i = aux; i < longitud; i++)
     {
-        fputc(*ptr,flujo);
+        fputc(*ptr, flujo);
         ptr++;
-        if((i%3) == 2 && i != longitud - 1)
-            fputc('.',flujo);
+        if ((i % 3) == 2 && i != longitud - 1)
+            fputc('.', flujo);
     }
     return ptr;
 }
