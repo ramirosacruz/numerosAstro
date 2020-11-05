526509e196d6e26c0bdc8579996f915bc1f580f4
Ramiro <40445502+ramirosacruz@users.noreply.github.com>
2020-11-04 22:15:20 -0300

    Revert "Docs"

--- run-tests.sh
+++
@@ -1,9 +0,0 @@
-gcc src/test/tst_arithmetic.c && ./a.out
-
-gcc src/test/tst_astroFile.c && ./a.out
-
-gcc src/test/tst_errorHandling.c && ./a.out
-
-gcc src/test/tst_numeroAstronomico.c && ./a.out
-
-gcc src/test/tst_general.c && ./a.out
